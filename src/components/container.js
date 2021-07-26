f69172c3c4a9a2b0a417338edda7446c6e25443a
Kartik Nair <kartikajitnair@gmail.com>
2021-07-26 16:01:00 +0400

    Fix bug with passing prop to DOM

--- src/components/container.js
+++ src/components/container.js
@@ -1,16 +1,19 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 
-const Container = props => (
-  <main
-    css={css`
-      max-width: ${props.maxWidth};
-      margin: 10rem auto 0 auto;
-    `}
-    {...props}
-  >
-    {props.children}
-  </main>
-)
+const Container = props => {
+  const { maxWidth, ...otherProps } = props
+  return (
+    <main
+      css={css`
+        max-width: ${maxWidth};
+        margin: 10rem auto 0 auto;
+      `}
+      {...otherProps}
+    >
+      {props.children}
+    </main>
+  )
+}
 
 export default Container
