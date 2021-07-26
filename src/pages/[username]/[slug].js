1b3a79178e9899443df386347bb7dd1e42a993f6
Kartik Nair <kartikajitnair@gmail.com>
2021-07-26 14:05:29 +0400

    Styling for profile and post pages

--- src/pages/[username]/[slug].js
+++ src/pages/[username]/[slug].js
@@ -1,22 +1,76 @@
+/** @jsxImportSource @emotion/react */
 import firebase from 'firebase'
+import { css } from '@emotion/react'
 
 import { getPostByUsernameAndSlug } from '../../lib/db'
 import FIREBASE_CONIFG from '../../lib/firebase-config'
+import theme from '../../lib/theme'
+
+import Container from '../../components/container'
+import Link from 'next/link'
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(FIREBASE_CONIFG)
 }
 
 export default function Post({ post }) {
+  console.log(post)
   return (
-    <main>
-      <h1>{post.title}</h1>
-      <p>
-        By {post.author} | Last edited:{' '}
-        {new Date(post.lastEdited).toDateString()}
-      </p>
-      <p>{post.content}</p>
-    </main>
+    <Container maxWidth="640px">
+      <h1
+        css={css`
+          font-size: 1.5rem;
+          letter-spacing: -0.03em;
+          margin-bottom: 2rem;
+        `}
+      >
+        {post.title}
+      </h1>
+
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          color: ${theme.colors.grey[3]};
+        `}
+      >
+        <img
+          src={post.author.photo}
+          alt="Profile picture"
+          css={css`
+            width: 2rem;
+            border-radius: 1rem;
+            margin-right: 1rem;
+          `}
+        />
+        <p>
+          <Link href={`/${post.author.name}`}>
+            <a
+              style={{
+                color: 'inherit',
+                textDecoration: 'none',
+                borderBottom: `1px dotted ${theme.colors.grey[2]}`,
+              }}
+            >
+              {post.author.displayName}
+            </a>
+          </Link>{' '}
+          / {new Date(post.lastEdited).toDateString()}
+        </p>
+      </div>
+
+      <div
+        css={css`
+          color: ${theme.colors.grey[4]};
+          margin-top: 2rem;
+          font-size: 1.125rem;
+          font-family: 'Newsreader', serif;
+          line-height: 1.5em;
+        `}
+      >
+        <p>{post.content}</p>
+      </div>
+    </Container>
   )
 }
 
@@ -41,6 +95,12 @@ export async function getStaticProps({ params }) {
     if (!post.published) {
       return { notFound: true }
     }
+    const userDoc = await firebase
+      .firestore()
+      .collection('users')
+      .doc(post.author)
+      .get()
+    post.author = userDoc.data()
     post.lastEdited = post.lastEdited.toDate().getTime()
     return {
       props: { post, revalidate: 1 },
--- src/pages/[username]/index.js
+++ src/pages/[username]/index.js
@@ -1,32 +1,104 @@
+/** @jsxImportSource @emotion/react */
 import firebase from 'firebase'
 import Link from 'next/link'
+import { css } from '@emotion/react'
 
 import { getUserByName } from '../../lib/db'
 import FIREBASE_CONIFG from '../../lib/firebase-config'
+import theme from '../../lib/theme'
+
+import Container from '../../components/container'
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(FIREBASE_CONIFG)
 }
 
+const truncate = (words, maxWords) => {
+  const split = words.split(' ')
+
+  if (split.length <= maxWords) return words
+
+  return split.slice(0, maxWords).join(' ') + '...'
+}
+
 export default function Profile({ user }) {
+  console.log(user)
+
   return (
-    <main>
-      <img src={user.photo} />
-      <h1>{user.displayName}'s profile page!</h1>
-      <h2>Posts:</h2>
-      <ul>
+    <Container maxWidth="640px">
+      <img
+        src={user.photo}
+        css={css`
+          width: 5rem;
+          border-radius: 2.5rem;
+        `}
+      />
+      <h1
+        css={css`
+          font-size: 1.5rem;
+          letter-spacing: -0.03em;
+          margin: 1.5rem 0 1rem 0;
+        `}
+      >
+        {user.displayName}
+      </h1>
+
+      <p
+        css={css`
+          color: ${theme.colors.grey[4]};
+          font-size: 1.125rem;
+          font-family: 'Newsreader', serif;
+          line-height: 1.5em;
+        `}
+      >
+        {user.about}
+      </p>
+
+      <ul
+        css={css`
+          list-style: none;
+          margin-top: 3rem;
+        `}
+      >
         {user.posts.map(post => (
           <li key={post.id}>
             <Link href={`/${user.name}/${post.slug}`}>
-              <a>
-                <h3>{post.title}</h3>
+              <a style={{ textDecoration: 'none', color: 'inherit' }}>
+                <h3
+                  css={css`
+                    font-size: 1rem;
+                    font-weight: 400;
+                  `}
+                >
+                  {post.title}
+                </h3>
               </a>
             </Link>
-            <p>{post.excerpt}</p>
+
+            <p
+              css={css`
+                margin: 0.75rem 0;
+                font-size: 0.9rem;
+                color: ${theme.colors.grey[3]};
+              `}
+            >
+              {new Date(post.lastEdited).toDateString()}
+            </p>
+
+            <p
+              css={css`
+                max-width: 25rem;
+                color: ${theme.colors.grey[4]};
+                font-family: 'Newsreader', serif;
+                line-height: 1.5em;
+              `}
+            >
+              {post.excerpt || truncate(post.content, 25)}
+            </p>
           </li>
         ))}
       </ul>
-    </main>
+    </Container>
   )
 }
 
