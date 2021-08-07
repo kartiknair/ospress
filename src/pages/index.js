35fa34ae6d33c1223206192fc394989290889412
Kartik Nair <kartikajitnair@gmail.com>
2021-08-07 12:03:57 +0400

    Add logo and copy to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,17 +29,60 @@ export default function Home() {
   }
 
   return (
-    <>
+    <div>
+      <div
+        css={css`
+          margin-top: 0rem;
+          margin-bottom: 2.5rem;
+
+          @media (max-width: 720px) {
+            margin-bottom: 10rem;
+          }
+
+          width: 2rem;
+          height: 2rem;
+
+          background-image: url('/images/logo-light.png');
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: 2rem;
+
+          html[data-theme='dark'] & {
+            background-image: url('/images/logo-dark.png');
+          }
+        `}
+      ></div>
       <h1
         css={css`
           font-size: 1.5rem;
           letter-spacing: -0.02rem;
-          margin-top: 15rem;
           margin-bottom: 1.5rem;
         `}
       >
-        Ultra minimal blogging platform for anybody who writes
+        An ultra-minimal platform for anybody who writes
       </h1>
+      <ul
+        css={css`
+          list-style: none;
+          color: var(--grey-3);
+          margin-bottom: 2.5rem;
+
+          li {
+            margin: 0.75rem 0;
+          }
+
+          li::before {
+            display: inline-block;
+            content: '';
+            font-size: 0.9rem;
+            margin-right: 0.5rem;
+          }
+        `}
+      >
+        <li>No ads</li>
+        <li>No paywalls</li>
+        <li>Open-source</li>
+      </ul>
       {loading ? (
         <Button>
           <Spinner />
@@ -86,7 +129,7 @@ export default function Home() {
           Sign in with Google
         </Button>
       )}
-    </>
+    </div>
   )
 }
 
